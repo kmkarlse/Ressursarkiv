50f843d9be6561fb59ea105df5fde5862e46969b
Karlsen <kmkarlse@hiof.no>
2023-03-06 10:43:38 +0100

    changes

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,10 +1,5 @@
 
 
-export default function ContentCard({title, ingress}) {
-    return (
-        <article className="content-card">
-            <h3>{title}</h3>
-            <p>{ingress}</p>
-        </article>
-    )
+export default function ContentCard({title}) {
+    return <h3>{title}</h3>
 }
